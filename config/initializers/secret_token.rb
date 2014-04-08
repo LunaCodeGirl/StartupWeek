be021e4db07a32faa2699ad3e4d2f09dd5651096
Spencer Comerford <Spencevail@gmail.com>
2014-04-07 18:57:25 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StartupWeek::Application.config.secret_key_base = '0cdcf3375f4279d28ae3978c04523342fc9070a9324f7a9a2c06d1dd6b60c8b2c8b1bf8ad0a9cd66c6abfe3fd1262f3a5765b6a46bcfc2dd768313fe9907ba97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StartupWeek::Application.config.session_store :cookie_store, key: '_StartupWeek_session'
