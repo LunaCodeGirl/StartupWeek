95a1aaa0304cf8b69f13b39c011700df9d49eef1
Spencer Comerford <Spencevail@gmail.com>
2014-04-07 22:04:10 -0600

    Created Application  model

--- db/migrate/20140408011011_create_applications.rb
+++ db/migrate/20140408011011_create_applications.rb
@@ -1,7 +1,11 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :class_level
+      t.string :email
+      t.text :description
       t.timestamps
     end
   end
